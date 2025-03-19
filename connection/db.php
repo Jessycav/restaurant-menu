f0624f6a0168805b8aec0553fbf7dae34feb1af6
Jessycav <jessyca.vannier@gmail.com>
2025-03-19 06:03:21 +0100

    update

--- connection/db.php
+++ connection/db.php
@@ -1,7 +1,15 @@
 <?php
-$mongoUri = "mongodb://127.0.0.1:27017"; //local
-//$mongoUri = getenv("MONGO_DB_URI"); //hébergement sur MongoDB Atlas
-$mongoClient = new MongoDB\Driver\Manager($mongoUri);
+//$mongoUri = "mongodb://127.0.0.1:27017"; //local
+$mongoUri = "mongodb+srv://JessV:RestauMenu2025@restaurant-menu.vatgq.mongodb.net/?retryWrites=true&w=majority&appName=restaurant-menu"; //hébergement sur MongoDB Atlas
+
+try {
+    $mongoClient = new MongoDB\Driver\Manager($mongoUri);
+    echo "connexion reussie à atlas";
+} catch (Exception $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+
+}
+
 $database = "restaurantDB";
 $collection = "menu";
 ?>
